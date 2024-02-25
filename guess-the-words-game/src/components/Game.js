7656b2e87755b72a6cbf2a3c51d83703f768de03
Tamwa Kamga Brandon Duclerc <duclerc.tamwa@facsciences-uy1.cm>
2024-02-25 19:15:44 +0100

    version V1 mis  a jour
    la version V1 est terminer on passe a la version V2

--- guess-the-words-game/src/components/Game.js
+++ guess-the-words-game/src/components/Game.js
@@ -66,7 +66,7 @@ const Game = () => {
 
   return (
     <div id="game">
-      <h1>Jeu du pendu</h1>
+      <h1>GuessTheWord - Game</h1>
 
       {currentWord !== null &&
         <Heart
@@ -102,6 +102,30 @@ const Game = () => {
       {(currentWord === null || win !== 0) &&
         <button id="play_new_game" onClick={launchNewGame}>Nouvelle partie</button>
       }
+
+      <footer>
+      <p>Coding by <strong>TAMWA KAMGA BRANDON DUCLERC ~alias~ BrandonIscoding </strong> </p>
+      <div className="footer-links">
+        <div className="footer-link">
+          <a href="https://wa.me/+237620510564">WhatsApp</a>
+        </div>
+        <div className="footer-link">
+          <a href="https://whatsapp.com/channel/0029Va8yLe56rsQskYGPN13s">Chaîne WhatsApp</a>
+        </div>
+        <div className="footer-link">
+          <a href="https://t.me/BrandonIsCoding">Telegram</a>
+        </div>
+        <div className="footer-link">
+          <a href="http://t.me/webmobilegeneralprogramming2023">Chaîne Telegram</a>
+        </div>
+        <div className="footer-link">
+          <a href="https://www.linkedin.com/in/brandon-duclerc-tamwa-kamga-3546a2291">LinkedIn</a>
+        </div>
+        <div className="footer-link">
+          <a href="mailto:brandoniscoding4@gmail.com">brandoniscoding4@gmail.com</a>
+        </div>
+      </div>
+      </footer>
     </div>
   );
 };
